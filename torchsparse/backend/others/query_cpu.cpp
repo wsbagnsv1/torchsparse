1d11b1cedc4a6a410efe7bdfea2306732138dd40
wsbagnsv1 <sclumpfpapa36@gmail.com>
2025-06-02 21:10:02 +0200

    Update query_cpu.cpp

--- torchsparse/backend/others/query_cpu.cpp
+++ torchsparse/backend/others/query_cpu.cpp
@@ -1,12 +1,13 @@
 #include "query_cpu.h"
-
 #include <torch/torch.h>
-
 #include <cmath>
-#include <google/dense_hash_map>
+#ifdef _WIN32
+    #include <unordered_map>
+#else
+    #include <google/dense_hash_map>
+#endif
 #include <iostream>
 #include <vector>
-
 #include "../hashmap/hashmap_cpu.hpp"
 
 at::Tensor hash_query_cpu(const at::Tensor hash_query,
@@ -14,24 +15,32 @@ at::Tensor hash_query_cpu(const at::Tensor hash_query,
                           const at::Tensor idx_target) {
   int n = hash_target.size(0);
   int n1 = hash_query.size(0);
-
+  
+#ifdef _WIN32
+  std::unordered_map<int64_t, int64_t> hashmap;
+#else
   google::dense_hash_map<int64_t, int64_t> hashmap;
   hashmap.set_empty_key(0);
+#endif
+
   at::Tensor out = torch::zeros(
       {n1}, at::device(hash_query.device()).dtype(at::ScalarType::Long));
+      
+  // Build hashmap
   for (int idx = 0; idx < n; idx++) {
     int64_t key = *(hash_target.data_ptr<int64_t>() + idx);
     int64_t val = *(idx_target.data_ptr<int64_t>() + idx) + 1;
-    hashmap.insert(std::make_pair(key, val));
+    hashmap[key] = val;
   }
+  
+  // Query hashmap
 #pragma omp parallel for
   for (int idx = 0; idx < n1; idx++) {
     int64_t key = *(hash_query.data_ptr<int64_t>() + idx);
-    google::dense_hash_map<int64_t, int64_t>::iterator iter = hashmap.find(key);
+    auto iter = hashmap.find(key);
     if (iter != hashmap.end()) {
       *(out.data_ptr<int64_t>() + idx) = iter->second;
     }
   }
-
   return out;
 }
